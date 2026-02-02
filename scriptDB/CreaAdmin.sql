27d7610dfdec45a6facd342a9be363c8a9b1dd3d
Véro Grué <veronique.gru@educa.jcyl.com>
2026-02-02 16:34:09 +0100

    Fix: Corrección de un error en el codigo sql

--- scriptDB/CreaAdmin.sql
+++ scriptDB/CreaAdmin.sql
@@ -1,3 +1,3 @@
-INSERT INTO T01_Usuario (T01_CodUsuario,T01_Password,T01_DescUsuario,T01_ImagenUsuario)
+INSERT INTO T01_Usuario (T01_CodUsuario,T01_Password,T01_DescUsuario,T01_Perfil,T01_ImagenUsuario)
                 VALUES
-            ('admin',SHA2('adminpaso',256),'Administrador','administrador');
+            ('admin',SHA2('adminpaso',256),'Administrador','administrador',null);
